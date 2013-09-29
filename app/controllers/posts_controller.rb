179c89356fc35c0b282d89de1a9c116b53dd24a8
Pattamon <theprapakarn@gmail.com>
2013-09-29 13:45:26 +0200

    Add Model Validate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post].permit(:title, :text))
 
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
+    @post.save
   end
 
   def show
