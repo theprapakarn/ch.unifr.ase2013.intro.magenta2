c4b1f5255315879c4cb6d72bc18d00e73fc9f165
Pattamon <theprapakarn@gmail.com>
2013-09-29 15:21:04 +0200

    Fix error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  
+
   def new
+    @post = Post.new
   end
 
   def index
@@ -10,8 +11,11 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post].permit(:title, :text))
     
-   @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
