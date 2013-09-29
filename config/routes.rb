ffdc1e90ab07e4f3d04c20fc0e3235299b8da0c9
Pattamon <theprapakarn@gmail.com>
2013-09-30 00:38:12 +0200

    Update Model

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Blog::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'posts/:id' => 'posts#show'
   get 'posts' => 'posts#index'
- 
+  post 'posts/:id' => 'posts#update
+
  # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
