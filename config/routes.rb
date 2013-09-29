4577782b16ed20658d099965ee8f0152c9af1417
Pattamon <theprapakarn@gmail.com>
2013-09-29 09:50:19 +0200

    Route to show.html

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Blog::Application.routes.draw do
  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  post GET '/posts/:id' => 'posts#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
