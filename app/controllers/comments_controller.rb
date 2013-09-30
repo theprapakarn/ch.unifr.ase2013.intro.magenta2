75113f70e29f7f7cb9818de6e1f8888ee1c57e5c
Pattamon <theprapakarn@gmail.com>
2013-09-30 20:58:11 +0200

    Add Comment Model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
-  
+ 
   def create
     @post = Post.find(params[:post_id])
     
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
-    redirect_to post_path(@post) 
+    redirect_to post_path(@post)
+  end 
 end
