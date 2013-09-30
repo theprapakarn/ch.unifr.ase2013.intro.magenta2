8ac8deaa582c714582bdd4ef02baf4baa43b7b45
Pattamon <theprapakarn@gmail.com>
2013-09-30 20:15:57 +0200

    Delete Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_path
+  end
+
   def edit
     @post = Post.find(params[:id])
   end  
