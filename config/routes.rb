96b16486dcfcccbb6e401f057a3ecf445b2a20de
Pattamon <theprapakarn@gmail.com>
2013-09-30 19:44:33 +0200

    Fix Posts List

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Blog::Application.routes.draw do
  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'posts/:id' => 'posts#show'
-  get 'posts' => 'posts#index'
-  get 'posts/:id/edit' => 'posts#edit'
-  delete 'posts/:id' => 'posts#destroy'
 
  # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
