75e05bbdbb762cb6c4a07336b8d23e99f5a1ecef
Pattamon <theprapakarn@gmail.com>
2013-09-30 20:40:34 +0200

    Add Comment Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Blog::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
- 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
